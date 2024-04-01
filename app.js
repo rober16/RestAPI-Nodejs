fcb1dc21a2cc535850bc5be87fa3a0b126032794
Roberto Gomez <robertoezequiel@live.com.ar>
2024-04-01 06:09:04 -0300

    Se modificaron los endpoints

--- app.js
+++ app.js
@@ -8,10 +8,17 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json()); // Middleware para procesar los datos en formato JSON
 app.disable('x-powered-by') //deshabilitar el header x-powered-by: Express
 
-
-// Ruta para obtener todas las tareas
+//Ruta para obtener todas las peliculas o filtrar por genero
 app.get('/movies', (req, res) => {
-    res.json(movies);
+    const {genre} = req.query
+    console.log(genre)
+    if(genre){
+        const filteredMovies = movies.filter(
+            movie => movie.genre.includes(genre)
+        )
+        return res.json(filteredMovies)
+    }
+    res.json(movies)
 });
 
 // Ruta para obtener una tarea por su ID
@@ -20,7 +27,9 @@ app.get('/movies/:id', (req, res) => {
     const movie = movies.find(movie => movie.id === id)
     if (movie) return res.json(movie)
     res.status(404).json({ message: 'Movie not found' })
-  })
+});
+
+
 
 // Iniciar el servidor
 app.listen(PORT, () => {
