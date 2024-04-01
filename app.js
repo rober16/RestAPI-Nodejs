30c9157576754f6b7b1140def9387b2b2d0b96d6
Roberto Gomez <robertoezequiel@live.com.ar>
2024-04-01 05:13:27 -0300

    Se crearon los endpoints para listar todas las peliculas y tambien filtrar por id

--- app.js
+++ app.js
@@ -1,57 +1,26 @@
+require('dotenv').config();
 const express = require('express');
+const movies = require('./movies.json');
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Middleware para procesar los datos en formato JSON
-app.use(express.json());
+app.use(express.json()); // Middleware para procesar los datos en formato JSON
+app.disable('x-powered-by') //deshabilitar el header x-powered-by: Express
 
-// Datos iniciales (simulados)
-let tasks = [
-    { id: 1, description: 'Hacer la compra' },
-    { id: 2, description: 'Estudiar Node.js' }
-];
 
 // Ruta para obtener todas las tareas
-app.get('/tasks', (req, res) => {
-    res.json(tasks);
+app.get('/movies', (req, res) => {
+    res.json(movies);
 });
 
 // Ruta para obtener una tarea por su ID
-app.get('/tasks/:id', (req, res) => {
+app.get('/movies/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    const task = tasks.find(task => task.id === id);
-    if (!task) {
-        return res.status(404).json({ message: 'Tarea no encontrada' });
-    }
-    res.json(task);
-});
+    const movie = movies.find(movie => movie.id === id);
+    if (!movie) return res.status(404).json({ message: 'Pelicula no encontrada' });
 
-// Ruta para crear una nueva tarea
-app.post('/tasks', (req, res) => {
-    const { description } = req.body;
-    const id = tasks.length + 1;
-    const newTask = { id, description };
-    tasks.push(newTask);
-    res.status(201).json(newTask);
-});
-
-// Ruta para actualizar una tarea existente
-app.put('/tasks/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const { description } = req.body;
-    const task = tasks.find(task => task.id === id);
-    if (!task) {
-        return res.status(404).json({ message: 'Tarea no encontrada' });
-    }
-    task.description = description;
-    res.json(task);
-});
-
-// Ruta para eliminar una tarea
-app.delete('/tasks/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    tasks = tasks.filter(task => task.id !== id);
-    res.status(204).end();
+    res.json(movie);
 });
 
 // Iniciar el servidor
