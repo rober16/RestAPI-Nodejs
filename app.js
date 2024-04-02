b47d85157bfd7fff69e964c86018c1f13d305546
Roberto Gomez <robertoezequiel@live.com.ar>
2024-04-02 02:23:16 -0300

    Se hizo una estructuracion para el manejo de rutas

--- app.js
+++ app.js
@@ -1,116 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const crypto = require('node:crypto');
-const movies = require('./movies.json');
-const cors = require('cors')
-const { validateMovie, validatePartialMovie } = require('./schemas/movies')
-
+import express, { json } from 'express';
+import { corsMiddleware } from './middlewares/cors.js';
+import { moviesRouter } from './routes/movies.js';
 const app = express();
-const PORT = process.env.PORT || 4000;
 
-app.use(express.json()); // Middleware para procesar los datos en formato JSON
-
-app.use(cors({
-    origin: (origin, callback) => {
-      const ACCEPTED_ORIGINS = [
-        'http://localhost:8080',
-        'http://localhost:4000'
-      ]
-  
-      if (ACCEPTED_ORIGINS.includes(origin)) {
-        return callback(null, true)
-      }
-  
-      if (!origin) {
-        return callback(null, true)
-      }
-  
-      return callback(new Error('Not allowed by CORS'))
-    }
-  }))
-
+app.use(json()); // Middleware para procesar los datos en formato JSON
+app.use(corsMiddleware())
 app.disable('x-powered-by') //deshabilitar el header x-powered-by: Express
 
-//Ruta para obtener todas las peliculas o filtrar por genero
-app.get('/movies', (req, res) => {
-    const {genre} = req.query
-    console.log(genre)
-    if(genre){
-        const filteredMovies = movies.filter(
-            movie => movie.genre.includes(genre)
-        )
-        return res.json(filteredMovies)
-    }
-    res.json(movies)
-});
+//Al acceder a /movies voy a cargar todas las rutas de moviesRouter
+app.get('/movies', moviesRouter);
 
-// Ruta para obtener una tarea por su ID
-app.get('/movies/:id', (req, res) => {
-    const { id } = req.params
-    const movie = movies.find(movie => movie.id === id)
-    if (movie) return res.json(movie)
-    res.status(404).json({ message: 'Movie not found' })
-});
+ // Iniciar el servidor
+const PORT = process.env.PORT || 4000;
 
-app.post('/movies/', (req, res) => {
-    const result = validateMovie(req.body)
-
-    if (!result.success) {
-        // 422 Unprocessable Entity
-        return res.status(400).json({ error: JSON.parse(result.error.message) })
-    }
-    // en base de datos
-    const newMovie = {
-        id: crypto.randomUUID(), // uuid v4
-        ...result.data
-    }
-
-    // Esto no sería REST, porque estamos guardando
-    // el estado de la aplicación en memoria
-    movies.push(newMovie)
-
-    res.status(201).json(newMovie)
-    // return status code 201 (resource created)
-});
-
-app.patch('/movies/:id', (req, res) => {
-    const result = validatePartialMovie(req.body)
-  
-    if (!result.success) {
-      return res.status(400).json({ error: JSON.parse(result.error.message) })
-    }
-  
-    const { id } = req.params
-    const movieIndex = movies.findIndex(movie => movie.id === id)
-  
-    if (movieIndex === -1) {
-      return res.status(404).json({ message: 'Movie not found' })
-    }
-  
-    const updateMovie = {
-      ...movies[movieIndex],
-      ...result.data
-    }
-  
-    movies[movieIndex] = updateMovie
-  
-    return res.json(updateMovie)
-})
-
-app.delete('/movies/:id', (req, res) => {
-  const { id } = req.params
-  const movieIndex = movies.findIndex(movie => movie.id === id)
-
-  if (movieIndex === -1) {
-    return res.status(404).json({ message: 'Movie not found' })
-  }
-
-  movies.splice(movieIndex, 1)
-
-  return res.json({ message: 'Movie deleted' })
-})
-
-// Iniciar el servidor
 app.listen(PORT, () => {
-    console.log(`Servidor iniciado en http://localhost:${PORT}`);
-});
+  console.log(`Servidor iniciado en http://localhost:${PORT}`);
+})
